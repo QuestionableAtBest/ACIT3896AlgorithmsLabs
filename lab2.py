6e64d8db0b3049450dc3f0909e2a337a43ff0f78
Harry Zhou <hzhou51@my.bcit.ca>
2024-01-17 14:24:22 -0800

    Lab 2 added

---
+++ lab2.py
@@ -0,0 +1,49 @@
+#Lab link:https://gist.github.com/jholman-bcit/90ef3d4c88acc798b93b5a21d87c5a91
+
+#Add 1 number to the beginning of a list that already has 10 numbers in it
+def add_one_to_ten():
+    pass
+#Paul's times:
+#Harry's times:
+
+#Add 1 number to the beginning of a list that already has 1,000,000 numbers in it
+def add_one_to_million():
+    pass
+#Paul's times:
+#Harry's times:
+
+#Add 1 number to the end of a list that already has 10 numbers in it
+def add_one_to_end_ten():
+    pass
+#Paul's times:
+#Harry's times:
+
+#Add 1 number to the end of a list that already has 1,000,000 numbers in it
+def add_one_to_end_million():
+    pass
+#Paul's times:
+#Harry's times:
+
+#Remove 1 number from the beginning of a list that already has 10 numbers in it
+def remove_one_from_ten():
+    pass
+#Paul's times:
+#Harry's times:
+
+#Remove 1 number from the beignning of a list that already has 1,000,000 numbers in it
+def remove_one_from_million():
+    pass
+#Paul's times:
+#Harry's times:
+
+#Remove 1 number from the end of a list that already has 10 numbers in it
+def remove_one_from_end_ten():
+    pass
+#Paul's times:
+#Harry's times:
+
+#Add 1 number to the beignning of a list that already has 10 numbers in it
+def remove_one_from_end_million():
+    pass
+#Paul's times:
+#Harry's times:
