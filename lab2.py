b75ad758ad37e3ce3e09c1364dfe879da8b49bb3
Paul Kim Set B <pkim59@my.bcit.ca>
2024-01-17 17:00:22 -0800

    First commit on Paul's

--- lab2.py
+++ lab2.py
@@ -1,8 +1,17 @@
 #Lab link:https://gist.github.com/jholman-bcit/90ef3d4c88acc798b93b5a21d87c5a91
 
+import time
+import random
+
 #Add 1 number to the beginning of a list that already has 10 numbers in it
 def add_one_to_ten():
-    pass
+    stuff = list(range(10))
+
+    t0 = time.perf_counter()
+    stuff.insert(0, random.randint(11, 20))
+    t1 = time.perf_counter()
+    
+    print("To add 1 number to the beginning of a list that already has 10 numbers in it took: ", t1-t0, "seconds")
 #Paul's times:
 #Harry's times:
 
